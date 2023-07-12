1d8253db4bf6f0939ef5cbcd80b020d73bb96c79
sevi-kun <dev@sevi-kun.me>
2023-07-12 22:09:00 +0200

    Remove unessesary keymap + switch to better filepicker keymap

--- lua/sevi-kun/remap.lua
+++ lua/sevi-kun/remap.lua
@@ -1,14 +1,7 @@
 vim.g.mapleader = " "
 
--- sevi-kun
 
-vim.keymap.set("n", "<C-n>", vim.cmd.bNext)
-vim.keymap.set("n", "<C-l>", vim.cmd.blast)
-
-
--- ThePrimeagen
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>.", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
